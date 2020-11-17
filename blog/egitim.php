514dddea57842d185636df6a47fff864dd5e98d8
olcayvarol <74182795+olcayvarol@users.noreply.github.com>
2020-11-17 17:32:36 +0300

    Add files via upload

---
+++ blog/egitim.php
@@ -0,0 +1,15 @@
+<?php
+include("header.php");
+?>
+<div class="container">
+
+<img src="meü.jpg" width="320" height="250">
+ 2019 devam etmekte Mersin Üniversitesi
+ <br>
+ <br><img src="baal.jpg" width="320" height="250">
+ 2014-2018 Niğde/Bor Atatürk Anadolu Liseli
+ <br>
+ <br><img src="orta.jpg" width="320" height="250">
+ 2006-2014 Niğde cumhuriyet ilk ve otaokulu
+
+</div>
---
+++ blog/index.php
@@ -0,0 +1,21 @@
+<?php
+include("header.php");
+?>
+
+<div class="container" >
+<img src="pp.jpg" width="320" height="250">
+<h2>Olcay varol</h2>
+<br>
+Doğum tarihi: 10 mayıs 2000
+<br>
+Okul: Mersin üniversitesi
+<br>
+
+
+
+  
+</div>
+
+</body>
+
+</html>
